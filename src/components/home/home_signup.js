2eaf475482b5421e8b15e7940fe2598b004b26cf
dannymcwaves <dannymcwaves96@gmail.com>
2018-03-15 19:54:33 +0000

    signup removed

---
+++ src/components/home/home_signup.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export default class HomeSignup extends Component {
+  render() {
+    return (
+      <div>this is the forms page for signing up</div>
+    )
+  }
+}
